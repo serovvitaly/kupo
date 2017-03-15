e7429146301ff693bab67b5bc4e0fc16d2280cf6
serovvitaly <serovvitaly@gmail.com>
2017-03-15 18:34:11 +0300

    Update run.py

--- spider/management/commands/run.py
+++ spider/management/commands/run.py
@@ -1,10 +1,18 @@
 import re
+import pickle
 import urllib.request
 from xml.dom import minidom
+from pymemcache.client.base import Client as MemClient
+
+mem_client = MemClient(('localhost', 11211))
 
 def get_urls():
-    print('Loading...')
     url = 'http://api.biglion.ru/api.php?method=get_torg_price&type=xml&ctype=all'
+    cache = mem_client.get(url)
+    if cache is not None:
+        print('Loading from cache...')
+        return pickle.loads(cache)
+    print('Loading from net...')
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as f:
             print('Parsing...')
@@ -15,7 +23,8 @@ def get_urls():
             urls_str = []
             for url_node in urls_nodels_list:
                 urls_str.append(url_node.firstChild.nodeValue.strip())
+            cache = mem_client.set(url,  pickle.dumps(urls_str))
             return urls_str
 
 
-print(get_urls())
+get_urls()
