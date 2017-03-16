a800150d0dd5b1f76fc62b9eb422ba46f85ddb6e
Vitaly Serov <serovvitaly@gmail.com>
2017-03-17 01:23:53 +0300

    Добавлены модели для спайдера + создано приложение для офферов

--- spider/admin.py
+++ spider/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from spider.models import OfferUrl, OfferProvider
 
-# Register your models here.
+class OfferUrlAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ['id', 'url_link', 'offer_provider', 'created_at']
+    list_filter = ['offer_provider']
+
+    def url_link(self, rec):
+        return '<a target="_blank" href="'+rec.url+'">'+rec.url+'</a>'
+    url_link.allow_tags = True
+
+
+class OfferProviderAdmin(admin.ModelAdmin):
+    ordering = ['title']
+    list_per_page = 20
+    list_display = ['title', 'damail_link', 'affiliate_program_link', 'provider']
+
+    def damail_link(self, rec):
+        return '<a target="_blank" href="http://'+rec.domain+'">'+rec.domain+'</a>'
+    damail_link.allow_tags = True
+
+    def affiliate_program_link(self, rec):
+        if rec.affiliate_program_url == '':
+            return ''
+        return '<a target="_blank" href="'+rec.affiliate_program_url+'">Партнерская программа</a>'
+    affiliate_program_link.allow_tags = True
+
+
+admin.site.register(OfferUrl, OfferUrlAdmin)
+admin.site.register(OfferProvider, OfferProviderAdmin)
--- spider/models.py
+++ spider/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class OfferUrl(models.Model):
+    class Meta:
+        db_table = 'offer_url'
+    url = models.TextField()
+    offer_provider = models.ForeignKey('OfferProvider')
+    is_supervised = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class OfferProvider(models.Model):
+    class Meta:
+        db_table = 'offer_provider'
+    title = models.CharField(max_length=300)
+    domain = models.CharField(max_length=100, default='', blank=True)
+    provider = models.CharField(max_length=300)
+    affiliate_program_url = models.CharField(max_length=200, default='', blank=True)
+    affiliate_program_conditions = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ContentProvider(models.Model):
+    class Meta:
+        db_table = 'content_provider'
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=800)
+
